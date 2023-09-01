96829504049e557b876a57242f1c96018341f59a
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-09-02 02:20:31 +0530

    optimized stock problem

--- LeetCode/stock.java
+++ LeetCode/stock.java
@@ -1,23 +1,14 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        int min,max,sum=0;
-        for(int i=0;i<arr.length;i++){
-            min=arr[i];
-            int temp=0;
-            for(int j=i+1;j<arr.length;j++){
-                max=arr[j];
-                if(max>min){
-                    temp=max-min;
-                    if(temp>sum){
-                        sum=temp;
-                    } else{
-                        continue;
-                    }
-                } else{
-                    continue;
-                }
-            }
-        } return sum;
-        } 
-       
-    }
+    public int maxProfit(int[] prices) {
+        if(prices.length==0 || prices.length==1){
+            return 0;
+        }
+        int maxVal=0;
+        int minVal=Integer.MAX_VALUE;
+     for(int i=0;i<prices.length;i++){
+        minVal=Math.min(minVal,prices[i]);
+        maxVal=Math.max(maxVal,prices[i]-minVal);
+     }
+     return maxVal;
+}
+}
