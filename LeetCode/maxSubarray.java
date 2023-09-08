acd9502e67a77b5e8fc8eb82fb9d6cfa5897092b
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-09-08 23:39:04 +0530

    modified file with correct code

--- LeetCode/maxSubarray.java
+++ LeetCode/maxSubarray.java
@@ -4,10 +4,10 @@ class Solution {
         int max=nums[0];
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            if(sum<0){
-                sum=0;
-            } if(sum>max){
+             if(sum>max){
                 max=sum;
+            } if(sum<0){
+                sum=0;
             }
            
         } return max;
