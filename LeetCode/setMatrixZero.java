d26a3f893e3da4b5813715955f5e82ec8c163f89
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-10-03 16:55:43 +0530

    optimized solution

--- LeetCode/setMatrixZero.java
+++ LeetCode/setMatrixZero.java
@@ -1,20 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int row=0;
-        int col=0;
+        boolean row=false;
+        boolean col=false;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    for (int m=0;m<matrix.length;m++){
-                        matrix[m][j]=0;
-                }
-                    for(int n=0;n<matrix[0].length;n++){
-                        matrix[i][n]=0;
-                    } 
+                    if(i==0) row=true;
+                    if(j==0) col=true;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                     
+                }
+            }
+        
+        } for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
-        
+        if(row){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
         }
     }
 }
